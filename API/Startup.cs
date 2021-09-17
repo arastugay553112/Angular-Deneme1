ab687f596b3f21dae43d07fd8f3c3de14011ca26
tugay aras <kes.ici@hotmail.com>
2021-09-17 08:42:09 +0300

    Mimari Altyapısı Kuruldu

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,10 +1,10 @@
-using API.Data.DataContext;
+using API.infrastructure.DataContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
