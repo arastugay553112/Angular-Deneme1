b8fba2632bca39a9316b034cf52dfcecae22f2d4
tugay aras <kes.ici@hotmail.com>
2021-09-23 09:11:21 +0300

    Specification classı oluşturuldu

---
+++ API.Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Core.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public BaseSpecification(Expression<Func<T,bool>> creteria)
+        {
+            Creteria = creteria;
+        }
+        public Expression<Func<T, bool>> Creteria { get; }
+
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        protected void AddIclude(Expression<Func<T,object>> IncludeExpression)
+        {
+            Includes.Add(IncludeExpression);
+        }
+    }
+}
